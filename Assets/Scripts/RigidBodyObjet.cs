230d9bb56a217723161f6fa8b6aa8acb245a9384
LorenzoPrince <lorenzoprince222@gmail.com>
2024-08-21 00:24:48 -0300

    ball jump
    Arregle problemas de fisicas y logre que se mueva de forma correcta la bola, ahora tambien puede saltar!!!.

--- Assets/Scripts/RigidBodyObjet.cs
+++ Assets/Scripts/RigidBodyObjet.cs
@@ -7,8 +7,10 @@ public class RigidBodyMut : MonoBehaviour
 
 {
     public float speed;
+    public float Jumpforce;
     public UnityEngine.Vector3 imputVector;
     public Rigidbody rigidBody;
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,5 +26,11 @@ public class RigidBodyMut : MonoBehaviour
 
         rigidBody.AddForce(imputVector.x * speed, 0f, imputVector.y * speed, ForceMode.Impulse);
 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.AddForce(0f, Jumpforce, 0f, ForceMode.Impulse); //si no esta el forcemode.impulse lo toma automatico
+                                                                      //como .force  (recomendado para motores, aceleracion gradual, etc) que hara que
+                                                                      //cada vez que toque incremente la velocidad de salto en cambio con este (impulse) la velocidad es de una.
+        }           
     }
 }
