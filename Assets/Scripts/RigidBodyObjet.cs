0c92bad987c11fff670e44c755e7808d2f58a711
LorenzoPrince <lorenzoprince222@gmail.com>
2024-09-10 12:58:34 -0300

    adelanto
    agregue y probe nuevas cosas, agregue kill zone y jugue con las escenas

--- Assets/Scripts/RigidBodyObjet.cs
+++ Assets/Scripts/RigidBodyObjet.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RigidBodyMut : MonoBehaviour  
 
@@ -10,12 +11,16 @@ public class RigidBodyMut : MonoBehaviour
     public float Jumpforce;
     public UnityEngine.Vector3 imputVector;
     public Rigidbody rigidBody;
-
+    public bool isGrounded;
+    public Collision contraLoQueChoque;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        isGrounded = true;
+
+
     }
 
     // Update is called once per frame
@@ -26,11 +31,29 @@ public class RigidBodyMut : MonoBehaviour
 
         rigidBody.AddForce(imputVector.x * speed, 0f, imputVector.y * speed, ForceMode.Impulse);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            
             rigidBody.AddForce(0f, Jumpforce, 0f, ForceMode.Impulse); //si no esta el forcemode.impulse lo toma automatico
-                                                                      //como .force  (recomendado para motores, aceleracion gradual, etc) que hara que
+            isGrounded = false;                                                          //como .force  (recomendado para motores, aceleracion gradual, etc) que hara que
                                                                       //cada vez que toque incremente la velocidad de salto en cambio con este (impulse) la velocidad es de una.
-        }           
+        }  
+      
+    }
+    private void OnCollisionEnter(Collision contraLoQueChoque)
+    {
+        Debug.Log("choque contra " + contraLoQueChoque.gameObject.name);
+        if (contraLoQueChoque.gameObject.CompareTag("pizo"))
+        {
+            isGrounded = true;
+        }
+        if(contraLoQueChoque.gameObject.CompareTag("KillZone")) 
+
+        {
+            Debug.Log("Kill me");
+
+            SceneManager.LoadScene(0);
+
+        }
     }
 }
